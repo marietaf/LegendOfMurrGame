40f13a16d035a2816f10aa1b1c99819f10e79e9d
Marieta Farova <marietafarova@gmail.com>
2013-05-13 17:59:43 -0400

    Add blank java project

---
+++ src/legendofmurrgame/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package legendofmurrgame;
+
+/**
+ *
+ * @author Marieta
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //WOOOHHH the game has begun!
+    }
+
+}
