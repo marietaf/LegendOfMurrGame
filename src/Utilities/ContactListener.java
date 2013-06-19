6f0eae0cc83459a54fc8ee2c2825dd4635f53b8f
Marieta F <marietafarova@gmail.com>
2013-06-18 23:48:09 -0400

    Added contact listener class

---
+++ src/Utilities/ContactListener.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Utilities;
+
+import org.jbox2d.callbacks.ContactImpulse;
+import org.jbox2d.collision.Manifold;
+import org.jbox2d.dynamics.World;
+import org.jbox2d.dynamics.contacts.Contact;
+
+/**
+ *
+ * @author Marieta
+ */
+public class ContactListener implements org.jbox2d.callbacks.ContactListener {
+
+    public void beginContact(Contact contact) {
+        if( ((contact.getFixtureA().getUserData() == "player") && (contact.getFixtureB().getUserData() == "transition")) ||
+            ((contact.getFixtureA().getUserData() == "transition") && (contact.getFixtureB().getUserData() == "player"))){
+            
+        }
+    }
+
+    public void endContact(Contact contact) {
+
+    }
+
+    public void preSolve(Contact contact, Manifold manifold) {
+
+    }
+
+    public void postSolve(Contact contact, ContactImpulse contactImpulse) {
+
+    }
+
+}
