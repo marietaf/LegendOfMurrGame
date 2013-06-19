8e2062142468cb3d78b20b0df2618024a4a77f4f
Aron <aron_yoo@hotmail.com>
2013-06-18 22:21:52 -0400

    Template for Transition State is HERE
    HUEHUEHUEHUEHUEHUEHUE

---
+++ src/States/Transition.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package States;
+
+/**
+ *
+ * @author Aron Yoo
+ */
+public class Transition extends BasicGameState{
+
+
+
+}
